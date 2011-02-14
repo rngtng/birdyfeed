959e64394dfa88416ca1c1b529f05424fd70adb9
Tobias Bielohlawek <tobi@soundcloud.com>
2011-02-14 15:24:08 +0100

    inited rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Birdyfeed::Application.config.secret_token = '0e1c0c96fab09a6e5230284c1c4a2af6ca1aad0f7d1dc8a357cfad9c53837aa1bdafecd8ff7919187e957c2f709df8b415ba10cd846c3341c3896810281daf76'
