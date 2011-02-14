43147e6dc2bfab5ee16d77fb99b14564519ee5b7
Tobias Bielohlawek <tobi@soundcloud.com>
2011-02-14 18:40:04 +0100

    added deploy script

---
+++ config/deploy.rb
@@ -0,0 +1,78 @@
+set :application, "birdyfeed"
+
+set :use_sudo, false
+set :rvm_type, :user
+
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+require "rvm/capistrano"
+set :rvm_ruby_string, 'ruby-1.9.2-rc2'
+
+# If you aren't deploying to /u/apps/#{application} on the target
+# servers (which is the default), you can specify the actual location
+# via the :deploy_to variable:
+set :deploy_to, "/kunden/warteschlange.de/produktiv/rails/#{application}"
+
+# If you aren't using Subversion to manage your source code, specify
+# your SCM below:
+set :scm, :git
+set :repository, "git://github.com/rngtng/birdyfeed.git"
+set :branch, "master"
+set :deploy_via, :remote_cache
+
+set :user, 'ssh-21560'
+set :ssh_options, { :forward_agent => true }
+
+role :app, "birdyfeed.warteschlange.de"
+role :web, "birdyfeed.warteschlange.de"
+role :db,  "birdyfeed.warteschlange.de", :primary => true
+
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  desc "Link in the production database.yml"
+  task :link_configs do
+    run "ln -nfs #{deploy_to}/#{shared_dir}/database.yml #{release_path}/config/database.yml"
+  end
+
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+end
+
+## Rails 3 updates:  http://rand9.com/blog/bundler-0-9-1-with-capistrano
+namespace :bundler do
+  task :create_symlink, :roles => :app do
+    shared_dir = File.join(shared_path, 'bundle')
+    release_dir = File.join(current_release, '.bundle')
+    run("mkdir -p #{shared_dir} && ln -s #{shared_dir} #{release_dir}")
+  end
+
+  task :bundle_new_release, :roles => :app do
+    bundler.create_symlink
+    run "cd #{release_path} && bundle install --without test"
+  end
+
+  task :lock, :roles => :app do
+    run "cd #{current_release} && bundle lock;"
+  end
+
+  task :unlock, :roles => :app do
+    run "cd #{current_release} && bundle unlock;"
+  end
+end
+
+def remote_file_exists?(full_path)
+  'true' ==  capture("if [ -e #{full_path} ]; then echo 'true'; fi").strip
+end
+
+
+# HOOKS
+after "deploy:update_code" do
+  bundler.bundle_new_release
+  deploy.link_configs
+end
+
