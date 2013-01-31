30ff9a7d53a565f25970b7a729854a2d7b29b501
Tobias Bielohlawek <tobi@soundcloud.com>
2013-02-01 01:54:03 +1100

    added auth

--- app/controllers/card_dav_controller.rb
+++ app/controllers/card_dav_controller.rb
@@ -1,4 +1,9 @@
 class CardDavController < ActionController::Base
+  REALM = "SuperSecret"
+  USERS = { "tobi" => Digest::MD5.hexdigest(["tobi", REALM, "asdasd"].join(":")) }
+
+  before_filter :authenticate
+
   # OPTIONS   -> options
   # HEAD      -> head
   # GET       -> show
@@ -45,6 +50,13 @@ class CardDavController < ActionController::Base
   #     response['Dav'] = dav_support.join(', ')
   #   end
   # end
+
+  def authenticate
+    authenticate_or_request_with_http_digest(REALM) do |username|
+      @current_username = username
+      USERS[username]
+    end
+  end
 
 
 end
