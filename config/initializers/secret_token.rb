948bb3b3f7104649707a5173f0ed900b654be281
Tobias Bielohlawek <tobi@soundcloud.com>
2014-12-12 12:38:36 +0100

    Update secret_token.rb

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Birdyfeed::Application.config.secret_token = '0122c0cfeb1358861d18ede6c70be0fd20a5d74268cf217757090d962df50469f0dc336dea0ae681c425a521fa93d67f24fb68ed2c46b62717c38859ed2d3b3e'
+Birdyfeed::Application.config.secret_token = ''
